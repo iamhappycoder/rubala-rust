3a7205702abb9e1f4dbae0e09082fc88e75c8c3e
Jasper V. Ferrer <iamhappycoder@icloud.com>
2025-05-01 14:33:58 +0800

    chore(comments): add copyright headers to source files

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,1 +1,12 @@
+/*
+ * Rubala
+ *
+ * Copyright (c) 2025, Jasper V. Ferrer
+ *
+ * This file is part of the Rubala source code.
+ *
+ * For licensing information, please see the LICENSE file distributed with this code.
+ * For inquiries or support, please visit the project's repository at https://github.com/iamhappycoder/rubala.
+ */
+
 pub mod router;
--- src/interfaces/http/controllers/mod.rs
+++ src/interfaces/http/controllers/mod.rs
@@ -1,3 +1,14 @@
+/*
+ * Rubala
+ *
+ * Copyright (c) 2025, Jasper V. Ferrer
+ *
+ * This file is part of the Rubala source code.
+ *
+ * For licensing information, please see the LICENSE file distributed with this code.
+ * For inquiries or support, please visit the project's repository at https://github.com/iamhappycoder/rubala.
+ */
+
 pub mod about_controller;
 pub mod home_controller;
 pub mod guest_book_controller;
